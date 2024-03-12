593a671fdb832b202e08de1226ae6687d8dd740f
Shakil Ahmed <shakil.ahmed@questionpro.com>
2024-03-12 11:12:54 +0600

    update test cov

--- src/app/__tests__/dummy.test.tsx
+++ src/app/__tests__/dummy.test.tsx
@@ -1,4 +1,4 @@
-import { addNumbersFromArray } from "../utils";
+import { addNumbersFromArray, findNumbers } from "../utils";
 
 describe("Page component", () => {
   it("should return 0 when an empty array is passed as argument", () => {
@@ -11,5 +11,10 @@ describe("Page component", () => {
     const numberArray: number[] = [1, 2, 3, 4, 5];
     const result = addNumbersFromArray(numberArray);
     expect(result).toBe(15);
+  });
+
+  it("should return an empty array when given an empty string", () => {
+    const result = findNumbers("");
+    expect(result).toEqual([]);
   });
 });
