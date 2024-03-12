ecfe669f8a2f7a60f1f6efe7d023fb2dedf5c477
Shakil Ahmed <shakil.ahmed@questionpro.com>
2024-03-12 10:08:56 +0600

    update test cov

--- src/app/__tests__/dummy.test.tsx
+++ src/app/__tests__/dummy.test.tsx
@@ -7,9 +7,9 @@ describe("Page component", () => {
     expect(result).toBe(0);
   });
 
-  // it("should return the sum of all numbers in the array when it has only positive numbers", () => {
-  //   const numberArray: number[] = [1, 2, 3, 4, 5];
-  //   const result = addNumbersFromArray(numberArray);
-  //   expect(result).toBe(15);
-  // });
+  it("should return the sum of all numbers in the array when it has only positive numbers", () => {
+    const numberArray: number[] = [1, 2, 3, 4, 5];
+    const result = addNumbersFromArray(numberArray);
+    expect(result).toBe(15);
+  });
 });
